b314d0260f1f12fb7c691a4dc11084acd62da0fd
Veytri <yogvey007@gmail.com>
2023-11-26 07:25:37 +0800

    Hotel Management App

---
+++ src/project/Project.java
@@ -0,0 +1,39 @@
+package project;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class Project extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            // load launcherScene.fxml in layout of type Parent
+            Parent root = FXMLLoader.load(getClass()
+                    .getResource("launcherScene.fxml"));
+            // Create obj of type scene , add layout in it
+            Scene scene = new Scene(root);
+            stage.initStyle(StageStyle.UNDECORATED);
+            
+            Image icon = new Image(getClass()
+                    .getResourceAsStream("/img/launcher_icon.png"));
+            
+            stage.getIcons().add(icon);
+            
+                
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception ex) {
+            System.out.println("Launcher Scene Error");
+        }
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
